66f09f30a3600c12f408e539651189d64dceae56
Shubham Gautam <sg2199203@gmail.com>
2021-03-18 00:55:42 +0530

    Styling up the website

---
+++ app.js
@@ -0,0 +1,17 @@
+var loader = document.querySelector('.loader')
+
+window.addEventListener('load', () => {
+    
+})
+
+navigator.getUserMedia = (navigator.getUserMedia || // use the proper vendor prefix
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+
+navigator.getUserMedia({ video: true }, function () {
+    loader.classList.add('hide-loader')
+}, function () {
+    alert('You need to give permission to use this service.')
+    location.reload()
+});
--- sketch.js
+++ sketch.js
@@ -1,49 +1,42 @@
-// Teachable Machine
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
-// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
-
 // The video
 let video;
 // For displaying the label
-let label = "waiting...";
+let label = "waiting..."
 // The classifier
-let classifier;
-let modelURL = 'https://teachablemachine.withgoogle.com/models/4pbv0d_vM/';
+let classifier
+let modelURL = 'https://teachablemachine.withgoogle.com/models/4pbv0d_vM/'
 
 // STEP 1: Load the model!
 function preload() {
-  classifier = ml5.imageClassifier(modelURL + 'model.json');
+  classifier = ml5.imageClassifier(modelURL + 'model.json')
 }
 
 
 function setup() {
-  createCanvas(640, 520);
+  createCanvas(640, 520)
   // Create the video
-  video = createCapture(VIDEO);
-  video.hide();
+  video = createCapture(VIDEO)
+  video.hide()
   // STEP 2: Start classifying
-  classifyVideo();
+  classifyVideo()
 }
 
 // STEP 2 classify the videeo!
 function classifyVideo() {
-  classifier.classify(video, gotResults);
+  classifier.classify(video, gotResults)
 }
 
 function draw() {
-  background(0);
+  background(0)
 
   // Draw the video
-  image(video, 0, 0);
+  image(video, 0, 0)
 
   // STEP 4: Draw the label
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  fill(255);
-  text(label, width / 2, height - 16);
-
-  // Pick an emoji, the "default" is train
+  textSize(32)
+  textAlign(CENTER, CENTER)
+  fill(255)
+  text(label, width / 2, height - 16)
   
 }
 
@@ -51,10 +44,10 @@ function draw() {
 function gotResults(error, results) {
   // Something went wrong!
   if (error) {
-    console.error(error);
-    return;
+    console.error(error)
+    return
   }
   // Store the label and classify again!
-  label = results[0].label;
-  classifyVideo();
+  label = results[0].label
+  classifyVideo()
 }
