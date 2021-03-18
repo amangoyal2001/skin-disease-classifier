a6e0f0723784e13c2649c4d6f5f41b1ef4c0080e
Shubham Gautam <sg2199203@gmail.com>
2021-03-18 10:42:27 +0530

    Styling up the website

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var loader = document.querySelector('.loader')
 
-navigator.getUserMedia = (navigator.getUserMedia || // use the proper vendor prefix
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
-
-navigator.getUserMedia({ video: true }, function () {
+window.addEventListener('load', () => {
     loader.classList.add('hide-loader')
-}, function () {
-    alert('You need to give permission to use this service.')
-    location.reload()
-});
+})
 
 var cross = document.querySelector('.cross')
 var popup = document.querySelector('.instructions')
