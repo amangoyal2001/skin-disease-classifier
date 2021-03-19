6199755e468b7f4cc446b967a41785ce914e9adb
Shubham Gautam <sg2199203@gmail.com>
2021-03-19 17:23:57 +0530

    building up

--- src/js/function.js
+++ src/js/function.js
@@ -3,3 +3,18 @@ var preLoader = document.querySelector('.pre-loader')
 window.addEventListener('load', () => {
     preLoader.classList.add('hide-loader')
 })
+
+var chooseBtns = document.querySelectorAll('.btn-choose')
+for (let i = 0; i < chooseBtns.length; i++) {
+    chooseBtns[i].addEventListener('click', () => {
+
+        $('.choose-pref').hide()
+         
+        if (chooseBtns[i].getAttribute('data-target') == 'import') {
+            $('.container').show()
+        }
+        else{
+            alert('hello2')
+        }
+    })
+}
