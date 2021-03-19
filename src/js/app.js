18cae39bcb3ef50af8fa32293231211af88a158e
Shubham Gautam <sg2199203@gmail.com>
2021-03-19 15:57:55 +0530

    building up

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,79 @@
+//document if ready
 $(document).ready(async function () {
-    $('.image-section').hide();
-    $('.loader').hide();
-    $('#result').hide();
 
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
+    // The classifier
+    let classifier
+    let modelURL = 'https://teachablemachine.withgoogle.com/models/4pbv0d_vM/'
+    let disease
+    let confidence
+
+    //function to get results
+    function fetchResults(error, results) {
+        
+        // Something went wrong!
+        if (error) return console.error(error)
+        
+        // Store the label and classify again!
+        disease = results[0].label
+        confidence = results[0].confidence * 100
+
+        alert(`${disease}: ${confidence.toFixed(2)}%`)
+    }
+    
+    //to preload the model
+    function preload() {
+        classifier = ml5.imageClassifier(modelURL + 'model.json')
+    }
+
+    //to classify the image
+    function classifyImage(img) {
+        classifier.classify(img, fetchResults)
+    }
+    
+    $('.image-section').hide()
+    $('#result').hide()
+
+    // preview the uploaded file
+    //reading the image as an URL
+    function readAsURL(dataImage) {
+        if (dataImage.files && dataImage.files[0]) {
+
+            //creating a fileReader
             var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').attr("src", e.target.result).hide().fadeIn(650);
+
+            reader.onload = (e) => {
+                $('#imagePreview').attr("src", e.target.result).hide().fadeIn(500);
             }
-            reader.readAsDataURL(input.files[0]);
+
+            //readAsDataURL function - pre-defined
+            reader.readAsDataURL(dataImage.files[0]);
         }
     }
 
+    //change event listener on upload
     $("#imageUpload").change(function () {
 
         $('.image-section').show();
         $('#btn-predict').show();
         $('#result').text('').hide();
-        readURL(this);
+        readAsURL(this);
+
     });
 
-    // Showing all possible results
-    function showResults(result) {
-        $('.loader').hide();
-        $('#result').fadeIn(600).text(result[0].className + '  -   ' + result[0].probability.toFixed(3));
-    }
-
-    // The classifier
-    var classifier
-    let modelURL = 'https://teachablemachine.withgoogle.com/models/4pbv0d_vM/'
-    var label
-
-    // STEP 1: Load the model!
-    function preload() {
-        classifier = ml5.imageClassifier(modelURL + 'model.json')
-    }
-
-    function gotResults(error, results) {
-        // Something went wrong!
-        if (error) {
-            console.error(error)
-            return
-        }
-        // Store the label and classify again!
-        label = results[0].label
-
-        console.log(label);
-    }
-
-    function classifyVideo(img) {
-        classifier.classify(img, gotResults)
-    }    
-
-    preload()
-    // Predict
+    //onclick event
     $('#btn-predict').click(async function () {
 
         // Show loading animation
         $(this).hide();
-        $('.loader').show();
 
         // Make a prediction through the model on our image.
-        const imgEl = document.getElementById('imagePreview');
-        classifyVideo(imgEl)
+        const imgFromUser = document.getElementById('imagePreview');
+
+        //sending image to classfiy function
+        classifyImage(imgFromUser)
 
     });
-
+    
+    //function to preload the model
+    preload()
 });
